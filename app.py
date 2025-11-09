4f9195837bc31f0b2bf33f1400a3b8de95cb8713
avasgpt <houseworkscoofficial@gmail.com>
2025-11-09 14:00:29 +0800

    Create app.py
    Planned VSharp. Contact me at +6593578864 or houseworkscoofficial@gmail.com

---
+++ app.py
@@ -0,0 +1,4 @@
+# it must be AI if we need to talk to it like a natural worker inless there are something like trigger words.
+# but after seeing the example comminication style, it cannot be through trigger words. Therefore we must use NLP for this programming languages.
+# Ill be haply to help develop VSharp.
+# Contact me at: houseworkscoofficial@gmail.com
