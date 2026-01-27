bf4934e29d802a09eb888fd4e0aeeb3abe91b7c7
avasgpt <houseworkscoofficial@gmail.com>
2026-01-27 11:45:02 +0800

    Update documentation.py
    added keywords() function for test keyword
    
    What to do next:
    Import Regex

--- documentation.py
+++ documentation.py
@@ -9,6 +9,19 @@ create_keyword="create";
 Command_keywords = 
 "
 [
-data_capture="Let's capture {{data}},{{data}},{{data}},";
+"Let's capture {{data}},{{data}},{{data}},"=data_capture;
 
 ]
+general_prog_functions = 
+"
+[
+
+]
+'''
+def keywords(test_keyword):'
+  test_keyword = "test"
+  return test_keyword
+
+local_test_val = keywords(test_keyword)
+
+print(local_test_val)
